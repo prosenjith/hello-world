9e57e9d032d8dd06bd4a604b805bca6f1126f1d5
prosenjith <royprosenjith@gmail.com>
2023-04-09 22:23:46 +0600

    Add API handler function

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "hello " + name,
+	})
+}
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/:name", IndexHandler)
 	router.Run()
 }
